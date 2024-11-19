a0f8a162da0da42d48b359397da3ff5cbb7f4085
Arin Paliwal <paliwalarin09@gmail.com>
2024-11-19 17:40:34 +0530

    Migrate to TS/TSX, add types, and optimize components

--- src/components/item-list.tsx
+++ src/components/item-list.tsx
@@ -1,30 +1,36 @@
 import { useState } from 'react';
 import { filterItems } from '../lib/items';
 import { toKebabCase } from '../lib/kebab-case';
-import Item from './item';
+import Item from './items';
+import { EmptyStateProps, ItemListProps, Item as ItemType } from '../types';
 
-const EmptyState = ({ id, items, filteredItems }) => (
+const EmptyState = ({ id }: EmptyStateProps) => (
   <p id={id} className="text-primary-400">
     (No items.)
   </p>
 );
 
-const ItemList = ({ title = 'Items', items, update, remove }) => {
-  const [filter, setFilter] = useState('');
+const ItemList = ({
+  title = 'Items',
+  items,
+  update,
+  remove,
+}: ItemListProps) => {
+  const [filter, setFilter] = useState<string>('');
   const id = toKebabCase(title);
 
   const filteredItems = filterItems(items, { name: filter });
   const isEmpty = !items.length;
 
   return (
-    <section id={id} className="w-full p-4 border-2 border-primary-200">
+    <section id={id} className="w-full border-2 border-primary-200 p-4">
       <header className="mb-4">
         <h2 className="text-lg font-semibold">{title}</h2>
         <label htmlFor={`${id}-filter`} className="hidden"></label>
         <input
           id={`${id}-filter`}
           placeholder="Filter"
-          className="w-full py-1 my-2"
+          className="my-2 w-full py-1"
           value={filter}
           onChange={(event) => setFilter(event.target.value)}
         />
